3bf112c7b457c9ebc2bdcf0ceaf54ed752dc2572
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-12-27 13:22:22 +0530

    Alarm System

---
+++ AlarmSystem.java
@@ -0,0 +1,135 @@
+
+
+
+
+
+
+
+
+
+
+
+
+import java.util.Random;
+
+class Alarm{
+    String alarmID;
+
+    public Alarm(String alarmID){
+        this.alarmID=alarmID;
+    }
+
+    public void trigerAlarm(int waterLevel){
+        if (waterLevel>=50){
+            System.out.println("Alarm "+alarmID+" On");
+        }else {
+            System.out.println("Alarm "+alarmID+" Off");
+        }
+    }
+}
+
+class Display{
+    public void showWaterLevel(int waterLevel){
+        System.out.println("Water level "+waterLevel);
+    }
+}
+
+class SMSManager{
+    public void sendSms(int waterLevel){
+        System.out.println("Send SMS "+waterLevel);
+    }
+}
+
+class ControlPanel{
+    private Alarm alarm;
+    private Display display;
+    private SMSManager smsManager;
+
+    private int waterLevel;
+
+
+    public ControlPanel() {
+    }
+
+    public ControlPanel(Alarm alarm, Display display, SMSManager smsManager, int waterLevel) {
+        this.alarm = alarm;
+        this.display = display;
+        this.smsManager = smsManager;
+        this.waterLevel = waterLevel;
+    }
+
+    public Alarm getAlarm() {
+        return alarm;
+    }
+
+    public void setAlarm(Alarm alarm) {
+        this.alarm = alarm;
+    }
+
+    public Display getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(Display display) {
+        this.display = display;
+    }
+
+    public SMSManager getSmsManager() {
+        return smsManager;
+    }
+
+    public void setSmsManager(SMSManager smsManager) {
+        this.smsManager = smsManager;
+    }
+
+    public int getWaterLevel() {
+        return waterLevel;
+    }
+
+    public void setWaterLevel(int waterLevel) {
+        this.waterLevel = waterLevel;
+    }
+
+    public void notifyObjects(){
+        this.alarm.trigerAlarm(waterLevel);
+        this.display.showWaterLevel(waterLevel);
+        this.smsManager.sendSms(waterLevel);
+
+    }
+
+    public void setData(int waterLevel){
+        if (this.waterLevel!=waterLevel){
+            notifyObjects();
+        }
+
+    }
+}
+
+
+public class AlarmSystem {
+    public static void main(String[] args) {
+
+        ControlPanel controlPanel=new ControlPanel();
+        controlPanel.setAlarm(new Alarm("Alarm 1"));
+        controlPanel.setDisplay(new Display());
+        controlPanel.setSmsManager(new SMSManager());
+
+
+        while (true){
+            Random random=new Random();
+            int randomNumber= random.nextInt(101);
+            controlPanel.setData(randomNumber);
+
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){
+                System.out.println(e);
+            }
+
+            System.out.println("=============================");
+        }
+
+
+
+    }
+}
